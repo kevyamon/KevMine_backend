15009b39ac093a631465664731f8b9a50c07e39b
baticlean <baticlean225@gmail.com>
2025-08-28 00:46:25 +0000

    4

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,13 +11,17 @@ import {
 } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 import { loginLimiter } from '../middleware/rateLimiter.js';
-import { validate, userRegisterSchema, userLoginSchema } from '../middleware/validationMiddleware.js';
+import {
+  validate,
+  userRegisterSchema,
+  userLoginSchema,
+} from '../middleware/validationMiddleware.js';
 
 const router = express.Router();
 
 router.post('/login', loginLimiter, validate(userLoginSchema), authUser);
 router.post('/register', validate(userRegisterSchema), registerUser);
-router.post('/logout', logoutUser);
+router.post('/logout', protect, logoutUser); // Le middleware 'protect' a été ajouté ici
 router.post('/forgot-password', forgotPassword);
 router.post('/refresh-token', refreshAccessToken); // New route for refresh token
 router.put('/reset-password/:resetToken', resetPassword);
